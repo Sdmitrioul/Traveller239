15b82d1a1ab745fb9315ea05f70a3487c4fb0b6a
”Dmitri <dmitri.zd1958@gmail.com>
2023-06-11 18:21:28 +0200

    backend added

---
+++ backend/src/test/java/com/traveller239/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveller239.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

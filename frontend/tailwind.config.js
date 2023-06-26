2771cffd70a3a3587e8f2e45dd1fef1627816108
”Dmitri <dmitri.zd1958@gmail.com>
2023-06-26 19:38:09 +0200

    Add basic js libraries for front

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      white: '#F6F6F6',
+      red: '#BA274A',
+      darkRed: '#841C26',
+      blue: '#2191FB',
+      light: '#B2ECE1',
+      aqua: '#8CDEDC'
+    },
+    fontFamily: {},
+    extend: {}
+  },
+  plugins: []
+}
